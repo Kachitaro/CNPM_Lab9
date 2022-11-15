56754c253dc375ee7d3db7ce90ec74fc3d9c6c5e
Kachitaro <anhtai741981@gmail.com>
2022-11-15 15:05:27 +0700

    testcases score

--- TestProject1/TestProject1/UnitTest1.cs
+++ TestProject1/TestProject1/UnitTest1.cs
@@ -1,15 +1,45 @@
 using StudentServiceLib;
+
+
 namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+        private string m;
+
         [TestMethod]
-        public void Test_value_()
+        public void Test_value_0()
         {
             Student s = new Student();
             s.Score = 0;
             Assert.AreEqual(0, s.Score);
+        }
+
+        [TestMethod]
+        public void Test_value_10()
+        {
+            Student s = new Student();
+            s.Score = 10;
+            Assert.AreEqual(10, s.Score);
+        }
+
+        [TestMethod]
+        public void Test_value_over10()
+        {
+            String ExpextedOver10 = "Score must not exeed 10.0";
+            String message = null ;
+            Student s = new Student();
+            try
+            {
+                s.Score = 11;
+
+            }catch(Exception e)
+            {
+                message = e.Message;
+            }
+            Assert.AreEqual(ExpextedOver10, message);
+            
         }
     }
 }
